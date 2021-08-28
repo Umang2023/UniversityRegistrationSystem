efe8407eaa82db13cd67bcdf7333128b2158fea6
Umang2023 <umangg.2023@gmail.com>
2021-08-28 19:54:43 +0530

    login page

---
+++ public/main.js
@@ -0,0 +1,26 @@
+const inputs = document.querySelectorAll(".input");
+
+function focusFunx() {
+    let parent = this.parentNode.parentNode;
+    parent.classList.add("focus");
+}
+
+inputs.forEach((input) => {
+    input.addEventListener("focus", focusFunx);
+});
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+const loginErrorMsg = document.getElementById("login-error-msg");
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
+
+    if (username === "user" && password === "pwd") {
+        alert("You have successfully logged in.");
+        location.reload();
+    } else {
+        loginErrorMsg.style.opacity = 1;
+    }
+})
