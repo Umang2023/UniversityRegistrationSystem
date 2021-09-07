c354e59f638652794b8d4283f1f0e6abb38c27fa
vikash452 <vikashp0901@gmail.com>
2021-09-07 12:01:51 +0530

    add course route added

--- routes/course.js
+++ routes/course.js
@@ -8,6 +8,32 @@ router.get('/current',authMiddleware , async (req,res)=>{
     // project aggregation
 })
 
+router.post('/addCourse' , authMiddleware , async (req,res)=>{
+    try{
+        const courseName=req.body.courseName
+        const courseID=req.body.courseID
+        const department=req.body.department
+        const totalSeats=req.body.totalSeats
+
+        const newAddedCourse=new Course({
+            courseName,
+            courseID,
+            department,
+            totalSeats,
+            remainingSeats:0,
+            faculty:req.body.faculty,
+            prerequisites:req.body.prerequisites
+        })
+
+        const savedCourse = await newAddedCourse.save();
+        return res.status(200).json({isError:false , savedCourse})
+
+    }catch(error)
+    {
+        return res.status(400).json(error.message)
+    }
+})
+
 router.put('/register',authMiddleware , async(req,res)=>{
     const courseID=req.body.courseID;
 
