c8b73693b1cba5a6cc8d50c62e136644f350e809
Umang2023 <umangg.2023@gmail.com>
2021-09-11 16:26:08 +0530

    Sign Up page

---
+++ public/js/signup.js
@@ -0,0 +1,42 @@
+const inputs = document.querySelectorAll(".input");
+
+function focusFunx() {
+    let parent = this.parentNode.parentNode;
+    parent.classList.add("focus");
+}
+
+inputs.forEach((input) => {
+    input.addEventListener("focus", focusFunx);
+});
+
+const signupForm = document.getElementById("signup-form");
+console.log(signupForm);
+
+const signupButton = document.getElementById("signup-form-submit");
+console.log(signupButton);
+signupButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = signupForm.username.value;
+    const email = signupForm.email.value;
+    const password = signupForm.password.value;
+    console.log(username, email, password);
+    // alert("Success");
+    fetch('/signup',{
+        method:"POST",
+        headers:{
+            'Content-Type':'application/json'
+        },
+        body:JSON.stringify({
+            email: email,
+            password: password,
+            name: username
+        })
+    })
+    .then(res => res.json())
+    .then((data) => {
+        console.log(data)
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+})
--- server.js
+++ server.js
@@ -1,36 +1,40 @@
-const express=require('express')
-const app=express();
-const bodyParser=require('body-parser')
-var PORT=process.env.PORT || 5000
+const express = require('express')
+const app = express();
+const bodyParser = require('body-parser')
+var PORT = process.env.PORT || 5000
 require('dotenv').config()
 const mongoose = require('mongoose')
 const cookieParser = require('cookie-parser')
 const authMiddleware = require('./middleware/authMiddlware')
 
 // mongoose.connect(process.env.MONGO_URL,{useNewUrlParser:true,useUnifiedTopology:true,useFindAndModify:false} )
-mongoose.connect('mongodb://localhost:27017/URS',{useNewUrlParser:true,useUnifiedTopology:true} );
+mongoose.connect('mongodb://localhost:27017/URS', { useNewUrlParser: true, useUnifiedTopology: true });
 
-mongoose.connection.on('connected',()=>{
+mongoose.connection.on('connected', () => {
     console.log('connected to database')
 })
-mongoose.connection.on('error',()=>{
+mongoose.connection.on('error', () => {
     console.log('failed to connect to database')
 })
 
 app.use(bodyParser.json())
 app.use(cookieParser())
-app.use('/user',require('./routes/user'))
-app.use('/course',require('./routes/course'))
+app.use('/user', require('./routes/user'))
+app.use('/course', require('./routes/course'))
 app.use(express.static('public'))
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/html/index.html')
 })
 
-app.get('/esehi',authMiddleware,(req,res)=>{
+app.get('/signup', (req, res) => {
+    res.sendFile(__dirname + '/public/html/signup.html')
+})
+
+app.get('/esehi', authMiddleware, (req, res) => {
     res.sendFile(__dirname + '/public/html/esehi.html')
 })
 
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log('server started on port 5000')
 })
